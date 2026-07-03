d3f461bda142e434e3f57603ead6d2158d5bf666
Richard <mattsson.richard@gmail.com>
2026-07-03 08:05:17 +0200

    Add deck image and update HTML structure; refactor JavaScript and TypeScript code for consistency

--- index.js
+++ index.js
@@ -1,57 +1,44 @@
-// Blackjack
-var _a;
-var createDeck = function () {
-    var deck = [];
-    var suits = ['HEARTS', 'SPADES', 'DIAMONDS', 'CLUBS'];
-    for (var n = 0; n < suits.length; n++) {
-        for (var i = 1; i <= 13; i++) {
+const suits = ["HEARTS", "SPADES", "DIAMONDS", "CLUBS"];
+function createDeck() {
+    let deck = [];
+    for (let n = 0; n < suits.length; n++) {
+        for (let i = 1; i < 13; i++) {
             deck.push({ suit: suits[n], value: i });
         }
     }
     return deck;
-};
-var shuffle = function (deck) {
-    // går igenom kortleken
-    for (var i = deck.length - 1; i > 0; i--) {
-        // skapar ett slumpat indextal som jag sparar undan i variabeln j
-        var j = Math.floor(Math.random() * (i + 1));
-        // sparar undan kortet som ska blandas temporärt i en varibel temp
-        var temp = deck[i];
-        // ändrar kortet till det slumpade indexet i variabeln j
-        deck[i] = deck[j];
-        // tar det slumpade kortet i variabeln j och låter det få det bytta kortets plats.
-        deck[j] = temp;
+}
+function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const randomNumber = Math.floor(Math.random() * (i + 1));
+        const tempSlot = deck[i];
+        deck[i] = deck[randomNumber];
+        deck[randomNumber] = tempSlot;
     }
     return deck;
-};
-var draw = function (deck) {
-    // Här inne vill jag ta bort ett kort från arrayen och skicka tillbaka den.
-    // då använder jag mig av metoden pop.
-    if (deck.length === 0) {
-        throw new Error('Deck is empty');
+}
+function draw(deck) {
+    const card = deck.pop();
+    if (!card) {
+        throw new Error("No cards left in deck");
     }
-    return deck.pop();
-};
-var score = function (card) {
-    // ska ta emot en array med kortobjekt
-    //  och räkna ut blackjack - poäng
-    // ess = 11 men 1 om poängen är större än 21
-    // klädda, J, Q, K = 10
-    // resterande värde är deras value = 2-10
-    var points = 0;
-    var aces = 0;
-    for (var i = 0; i < card.length; i++) {
-        if (card[i].value === 1) {
+    return card;
+}
+function score(cards) {
+    let points = 0;
+    let aces = 0;
+    for (let i = 0; i < cards.length; i++) {
+        if (cards[i].value === 1) {
             points += 11;
             aces++;
         }
-        else if (card[i].value === 11 ||
-            card[i].value === 12 ||
-            card[i].value === 13) {
+        else if (cards[i].value === 11 ||
+            cards[i].value === 12 ||
+            cards[i].value === 13) {
             points += 10;
         }
         else {
-            points += card[i].value;
+            points += cards[i].value;
         }
     }
     while (points > 21 && aces > 0) {
@@ -59,181 +46,173 @@ var score = function (card) {
         aces--;
     }
     return points;
-};
-var prettyCard = function (card) {
-    if (typeof card === 'string') {
+}
+function prettyCard(card) {
+    if (typeof card === "string") {
         return card;
     }
-    var suit = '', value = '';
+    let suit = "", value = "";
     switch (card.value) {
         case 1:
-            value = 'A';
+            value = "A";
             break;
         case 2:
-            value = '2';
+            value = "2";
             break;
         case 3:
-            value = '3';
+            value = "3";
             break;
         case 4:
-            value = '4';
+            value = "4";
             break;
         case 5:
-            value = '5';
+            value = "5";
             break;
         case 6:
-            value = '6';
+            value = "6";
             break;
         case 7:
-            value = '7';
+            value = "7";
             break;
         case 8:
-            value = '8';
+            value = "8";
             break;
         case 9:
-            value = '9';
+            value = "9";
             break;
         case 10:
-            value = '10';
+            value = "10";
             break;
         case 11:
-            value = 'J';
+            value = "J";
             break;
         case 12:
-            value = 'Q';
+            value = "Q";
             break;
         case 13:
-            value = 'K';
+            value = "K";
             break;
     }
     switch (card.suit) {
-        case 'HEARTS':
+        case "HEARTS":
             // suit = "\u2665"; Dessa koder var för tidigare uppgift utan kortbilder.
-            suit = 'H';
+            suit = "H";
             break;
-        case 'SPADES':
+        case "SPADES":
             // suit = "\u2660";
-            suit = 'S';
+            suit = "S";
             break;
-        case 'DIAMONDS':
+        case "DIAMONDS":
             // suit = "\u2666";
-            suit = 'D';
+            suit = "D";
             break;
-        case 'CLUBS':
+        case "CLUBS":
             // suit = "\u2663";
-            suit = 'C';
+            suit = "C";
             break;
     }
-    return value + '-' + suit;
-};
-var deck = createDeck();
-var shuffledDeck = shuffle(deck);
-var hitButton = document.querySelector('#hitButton');
-var dealButton = document.querySelector('#dealButton');
-var stayButton = document.querySelector('#stayButton');
-var playAgainButton = document.querySelector('#playAgainButton');
-var playerInputField = document.querySelector('#playerScore');
-var dealerInputField = document.querySelector('#dealerScore');
-var dealerOList = document.querySelector('#dealerOList');
-var oList = document.querySelector('#oList');
-var hand = [];
-var dealerHand = [];
-var handPoints = [];
-var dealerHandPoints = [];
-var result;
-var dealerResult;
-var listElement;
-var imgElement;
-var dealerListElement;
-var dealerImgElement;
-var endGame = false;
+    return value + "-" + suit;
+}
+const deck = createDeck();
+const shuffledDeck = shuffle(deck);
+const dealButton = document.querySelector("#dealButton");
+const stayButton = document.querySelector("#stayButton");
+const playerInputField = document.querySelector("#playerScore");
+const dealerInputField = document.querySelector("#dealerScore");
+const dealerOList = document.querySelector("#dealerOList");
+const oList = document.querySelector("#oList");
+let hand = [];
+let dealerHand = [];
+let handPoints = [];
+let dealerHandPoints = [];
+let result;
+let dealerResult;
+let listElement;
+let imgElement;
+let dealerListElement;
+let dealerImgElement;
+let startGame = false;
+let endGame = false;
 // För att lagra poängen när sidan laddas om.
-var SCORE_KEY = 'gameScore';
-var scoreField = document.querySelector('#points');
-var earnedPoints = parseInt((_a = sessionStorage.getItem(SCORE_KEY)) !== null && _a !== void 0 ? _a : '0') || 0;
+const SCORE_KEY = "gameScore";
+const scoreField = document.querySelector("#points");
+let earnedPoints = parseInt(sessionStorage.getItem(SCORE_KEY) ?? "0") || 0;
 if (scoreField instanceof HTMLInputElement) {
     scoreField.value = earnedPoints.toString();
 }
-var deal = function () {
-    if (endGame === true) {
+function clearTable() {
+    sessionStorage.setItem(SCORE_KEY, earnedPoints.toString());
+    location.reload();
+    endGame = false;
+}
+function deal() {
+    if (endGame) {
         clearTable();
     }
+    if (result >= 21) {
+        stay();
+        clearTable();
+    }
+    startGame = true;
     if (hand.length < 2) {
-        for (var i = 0; i < 2; i++) {
-            var card_1 = draw(shuffledDeck);
-            hand.push(card_1);
-            handPoints.push(card_1);
+        for (let i = 0; i < 2; i++) {
+            const card = draw(shuffledDeck);
+            hand.push(card);
+            handPoints.push(card);
         }
         result = score(handPoints);
-        if (playerInputField) {
-            playerInputField.setAttribute('value', result.toString());
-        }
-        for (var i = 0; i < hand.length; i++) {
+        playerInputField?.setAttribute("value", result.toString());
+        for (let i = 0; i < hand.length; i++) {
             hand[i] = prettyCard(hand[i]);
-            listElement = document.createElement('li');
-            imgElement = document.createElement('img');
-            if (oList) {
-                oList.appendChild(listElement);
-            }
+            listElement = document.createElement("li");
+            imgElement = document.createElement("img");
+            oList?.appendChild(listElement);
             listElement.appendChild(imgElement);
-            imgElement.setAttribute('src', 'cards/' + hand[i] + '.png');
+            imgElement.setAttribute("src", "cards/" + hand[i] + ".png");
         }
-        var card = draw(shuffledDeck);
+        const card = draw(shuffledDeck);
         dealerHand.push(card);
         dealerHandPoints.push(card);
         dealerResult = score(dealerHandPoints);
         dealerHand[0] = prettyCard(dealerHand[0]);
-        dealerListElement = document.createElement('li');
-        dealerImgElement = document.createElement('img');
-        if (dealerOList) {
-            dealerOList.appendChild(dealerListElement);
-        }
+        dealerListElement = document.createElement("li");
+        dealerImgElement = document.createElement("img");
+        dealerOList?.appendChild(dealerListElement);
         dealerListElement.appendChild(dealerImgElement);
-        dealerImgElement.setAttribute('src', 'cards/' + dealerHand[0] + '.png');
+        dealerImgElement.setAttribute("src", "cards/" + dealerHand[0] + ".png");
     }
     else {
-        var card = draw(shuffledDeck);
+        const card = draw(shuffledDeck);
         hand.push(card);
         handPoints.push(card);
         result = score(handPoints);
-        if (playerInputField) {
-            playerInputField.setAttribute('value', result.toString());
-        }
-        var i = hand.length - 1;
-        hand[i] = prettyCard(hand[i]);
-        listElement = document.createElement('li');
-        if (oList) {
-            oList.appendChild(listElement);
-        }
-        imgElement = document.createElement('img');
+        playerInputField?.setAttribute("value", result.toString());
+        const lastIndex = hand.length - 1;
+        hand[lastIndex] = prettyCard(hand[lastIndex]);
+        listElement = document.createElement("li");
+        oList?.appendChild(listElement);
+        imgElement = document.createElement("img");
         listElement.appendChild(imgElement);
-        imgElement.setAttribute('src', 'cards/' + hand[i] + '.png');
-        listElement.classList.add('cards');
+        imgElement.setAttribute("src", "cards/" + hand[lastIndex] + ".png");
+        listElement.classList.add("cards");
     }
-};
-// Döpte om Deal - button i spelet och slog ihop funktionen i deal.
-if (dealButton) {
-    dealButton.addEventListener('click', deal);
 }
-var stay = function () {
-    if (endGame === false) {
+dealButton?.addEventListener("click", deal);
+function stay() {
+    if (startGame && !endGame) {
         do {
-            var card = draw(shuffledDeck);
+            const card = draw(shuffledDeck);
             dealerHand.push(card);
             dealerHandPoints.push(card);
             dealerResult = score(dealerHandPoints);
-            if (dealerInputField) {
-                dealerInputField.setAttribute('value', dealerResult.toString());
-            }
-            var i = dealerHand.length - 1;
-            dealerHand[i] = prettyCard(dealerHand[i]);
-            dealerListElement = document.createElement('li');
-            if (dealerOList) {
-                dealerOList.appendChild(dealerListElement);
-            }
-            dealerImgElement = document.createElement('img');
+            dealerInputField?.setAttribute("value", dealerResult.toString());
+            const lastIndex = dealerHand.length - 1;
+            dealerHand[lastIndex] = prettyCard(dealerHand[lastIndex]);
+            dealerListElement = document.createElement("li");
+            dealerOList?.appendChild(dealerListElement);
+            dealerImgElement = document.createElement("img");
             dealerListElement.appendChild(dealerImgElement);
-            dealerImgElement.setAttribute('src', 'cards/' + dealerHand[i] + '.png');
+            dealerImgElement.setAttribute("src", "cards/" + dealerHand[lastIndex] + ".png");
         } while (dealerResult < 21 && dealerResult < result);
         if (result === 21 && hand.length === 2) {
             updatePoints(1);
@@ -252,22 +231,20 @@ var stay = function () {
         else {
             updatePoints(1);
         }
+        startGame = false;
+        endGame = true;
     }
     if (earnedPoints === 2) {
-        alert('You are the champion!');
+        alert("You are the champion!");
     }
-    endGame = true;
-};
-if (stayButton) {
-    stayButton.addEventListener('click', stay);
 }
-var updatePoints = function (change) {
+stayButton?.addEventListener("click", stay);
+function updatePoints(change) {
     earnedPoints += change;
+    console.log(earnedPoints);
+    if (scoreField instanceof HTMLInputElement) {
+        scoreField.value = earnedPoints.toString();
+    }
     sessionStorage.setItem(SCORE_KEY, earnedPoints.toString());
-    //points.value = earnedPoints; // Update the visible value directly
-};
-var clearTable = function () {
-    sessionStorage.setItem(SCORE_KEY, earnedPoints.toString());
-    location.reload();
-    endGame = false;
-};
+}
+export {};
--- index.ts
+++ index.ts
@@ -1,260 +1,250 @@
-// Blackjack
+export {};
+type Card = { suit: string; value: number };
+const suits = ["HEARTS", "SPADES", "DIAMONDS", "CLUBS"];
 
-let createDeck = function (): Card[] {
-  let deck: { suit: string; value: number }[] = [];
-  let suits = ['HEARTS', 'SPADES', 'DIAMONDS', 'CLUBS'];
+function createDeck(): Card[] {
+  let deck: Card[] = [];
 
   for (let n = 0; n < suits.length; n++) {
-    for (let i = 1; i <= 13; i++) {
+    for (let i = 1; i < 13; i++) {
       deck.push({ suit: suits[n], value: i });
     }
   }
   return deck;
-};
-let shuffle = function (deck: { suit: string; value: number }[]) {
-  // går igenom kortleken
+}
+
+function shuffle(deck: Card[]) {
   for (let i = deck.length - 1; i > 0; i--) {
-    // skapar ett slumpat indextal som jag sparar undan i variabeln j
-    let j = Math.floor(Math.random() * (i + 1));
-    // sparar undan kortet som ska blandas temporärt i en varibel temp
-    let temp = deck[i];
-    // ändrar kortet till det slumpade indexet i variabeln j
-    deck[i] = deck[j];
-    // tar det slumpade kortet i variabeln j och låter det få det bytta kortets plats.
-    deck[j] = temp;
+    const randomNumber = Math.floor(Math.random() * (i + 1));
+    const tempSlot = deck[i];
+    deck[i] = deck[randomNumber];
+    deck[randomNumber] = tempSlot;
   }
   return deck;
-};
+}
 
-type Card = { suit: string; value: number };
+function draw(deck: Card[]): Card {
+  const card = deck.pop();
 
-let draw = function (deck: { suit: string; value: number }[]): Card {
-  // Här inne vill jag ta bort ett kort från arrayen och skicka tillbaka den.
-  // då använder jag mig av metoden pop.
-
-  if (deck.length === 0) {
-    throw new Error('Deck is empty');
+  if (!card) {
+    throw new Error("No cards left in deck");
   }
 
-  return deck.pop() as Card;
-};
-let score = function (card: { suit: string; value: number }[]) {
-  // ska ta emot en array med kortobjekt
-  //  och räkna ut blackjack - poäng
-  // ess = 11 men 1 om poängen är större än 21
-  // klädda, J, Q, K = 10
-  // resterande värde är deras value = 2-10
+  return card;
+}
+
+function score(cards: Card[]) {
   let points = 0;
   let aces = 0;
 
-  for (let i = 0; i < card.length; i++) {
-    if (card[i].value === 1) {
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].value === 1) {
       points += 11;
       aces++;
     } else if (
-      card[i].value === 11 ||
-      card[i].value === 12 ||
-      card[i].value === 13
+      cards[i].value === 11 ||
+      cards[i].value === 12 ||
+      cards[i].value === 13
     ) {
       points += 10;
     } else {
-      points += card[i].value;
+      points += cards[i].value;
     }
   }
+
   while (points > 21 && aces > 0) {
     points -= 10;
     aces--;
   }
   return points;
-};
-let prettyCard = function (card: Card | string) {
-  if (typeof card === 'string') {
+}
+
+function prettyCard(card: Card | string) {
+  if (typeof card === "string") {
     return card;
   }
-  let suit: string = '',
-    value: string = '';
+  let suit = "",
+    value = "";
   switch (card.value) {
     case 1:
-      value = 'A';
+      value = "A";
       break;
     case 2:
-      value = '2';
+      value = "2";
       break;
     case 3:
-      value = '3';
+      value = "3";
       break;
     case 4:
-      value = '4';
+      value = "4";
       break;
     case 5:
-      value = '5';
+      value = "5";
       break;
     case 6:
-      value = '6';
+      value = "6";
       break;
     case 7:
-      value = '7';
+      value = "7";
       break;
     case 8:
-      value = '8';
+      value = "8";
       break;
     case 9:
-      value = '9';
+      value = "9";
       break;
     case 10:
-      value = '10';
+      value = "10";
       break;
     case 11:
-      value = 'J';
+      value = "J";
       break;
     case 12:
-      value = 'Q';
+      value = "Q";
       break;
     case 13:
-      value = 'K';
+      value = "K";
       break;
   }
   switch (card.suit) {
-    case 'HEARTS':
+    case "HEARTS":
       // suit = "\u2665"; Dessa koder var för tidigare uppgift utan kortbilder.
-      suit = 'H';
+      suit = "H";
       break;
-    case 'SPADES':
+    case "SPADES":
       // suit = "\u2660";
-      suit = 'S';
+      suit = "S";
       break;
-    case 'DIAMONDS':
+    case "DIAMONDS":
       // suit = "\u2666";
-      suit = 'D';
+      suit = "D";
       break;
-    case 'CLUBS':
+    case "CLUBS":
       // suit = "\u2663";
-      suit = 'C';
+      suit = "C";
       break;
   }
-  return value + '-' + suit;
-};
+  return value + "-" + suit;
+}
 
-let deck = createDeck();
-let shuffledDeck = shuffle(deck);
+const deck = createDeck();
+const shuffledDeck = shuffle(deck);
 
-const hitButton = document.querySelector('#hitButton');
-const dealButton = document.querySelector('#dealButton');
-const stayButton = document.querySelector('#stayButton');
-const playAgainButton = document.querySelector('#playAgainButton');
-const playerInputField = document.querySelector('#playerScore');
-const dealerInputField = document.querySelector('#dealerScore');
-const dealerOList = document.querySelector('#dealerOList');
-const oList = document.querySelector('#oList');
+const dealButton = document.querySelector("#dealButton");
+const stayButton = document.querySelector("#stayButton");
+const playerInputField = document.querySelector("#playerScore");
+const dealerInputField = document.querySelector("#dealerScore");
+const dealerOList = document.querySelector("#dealerOList");
+const oList = document.querySelector("#oList");
 
 let hand: (Card | string)[] = [];
 let dealerHand: (Card | string)[] = [];
-let handPoints: { suit: string; value: number }[] = [];
-let dealerHandPoints: { suit: string; value: number }[] = [];
+let handPoints: Card[] = [];
+let dealerHandPoints: Card[] = [];
 let result: number;
 let dealerResult;
 let listElement;
 let imgElement;
 let dealerListElement;
 let dealerImgElement;
+let startGame = false;
 let endGame = false;
 
 // För att lagra poängen när sidan laddas om.
-const SCORE_KEY = 'gameScore';
-const scoreField = document.querySelector('#points');
+const SCORE_KEY = "gameScore";
+const scoreField = document.querySelector("#points");
 let earnedPoints: number =
-  parseInt(sessionStorage.getItem(SCORE_KEY) ?? '0') || 0;
+  parseInt(sessionStorage.getItem(SCORE_KEY) ?? "0") || 0;
 if (scoreField instanceof HTMLInputElement) {
   scoreField.value = earnedPoints.toString();
 }
 
-let deal = function () {
-  if (endGame === true) {
+function clearTable() {
+  sessionStorage.setItem(SCORE_KEY, earnedPoints.toString());
+  location.reload();
+  endGame = false;
+}
+
+function deal() {
+  if (endGame) {
     clearTable();
   }
+  if (result >= 21) {
+    stay();
+    clearTable();
+  }
+  startGame = true;
   if (hand.length < 2) {
     for (let i = 0; i < 2; i++) {
-      let card: Card = draw(shuffledDeck);
+      const card: Card = draw(shuffledDeck);
       hand.push(card);
       handPoints.push(card);
     }
-    result = score(handPoints);
 
-    if (playerInputField) {
-      playerInputField.setAttribute('value', result.toString());
-    }
+    result = score(handPoints);
+    playerInputField?.setAttribute("value", result.toString());
 
     for (let i = 0; i < hand.length; i++) {
       hand[i] = prettyCard(hand[i]);
-      listElement = document.createElement('li');
-      imgElement = document.createElement('img');
-      if (oList) {
-        oList.appendChild(listElement);
-      }
+      listElement = document.createElement("li");
+      imgElement = document.createElement("img");
+      oList?.appendChild(listElement);
       listElement.appendChild(imgElement);
-      imgElement.setAttribute('src', 'cards/' + hand[i] + '.png');
+      imgElement.setAttribute("src", "cards/" + hand[i] + ".png");
     }
 
-    let card: Card = draw(shuffledDeck);
+    const card: Card = draw(shuffledDeck);
     dealerHand.push(card);
     dealerHandPoints.push(card);
     dealerResult = score(dealerHandPoints);
 
     dealerHand[0] = prettyCard(dealerHand[0]);
-    dealerListElement = document.createElement('li');
-    dealerImgElement = document.createElement('img');
-    if (dealerOList) {
-      dealerOList.appendChild(dealerListElement);
-    }
+    dealerListElement = document.createElement("li");
+    dealerImgElement = document.createElement("img");
+    dealerOList?.appendChild(dealerListElement);
     dealerListElement.appendChild(dealerImgElement);
-    dealerImgElement.setAttribute('src', 'cards/' + dealerHand[0] + '.png');
+    dealerImgElement.setAttribute("src", "cards/" + dealerHand[0] + ".png");
   } else {
-    let card = draw(shuffledDeck);
+    const card = draw(shuffledDeck);
     hand.push(card);
     handPoints.push(card);
-
     result = score(handPoints);
-    if (playerInputField) {
-      playerInputField.setAttribute('value', result.toString());
-    }
 
-    let i = hand.length - 1;
-    hand[i] = prettyCard(hand[i]);
-    listElement = document.createElement('li');
-    if (oList) {
-      oList.appendChild(listElement);
-    }
-    imgElement = document.createElement('img');
+    playerInputField?.setAttribute("value", result.toString());
+
+    const lastIndex = hand.length - 1;
+    hand[lastIndex] = prettyCard(hand[lastIndex]);
+    listElement = document.createElement("li");
+    oList?.appendChild(listElement);
+    imgElement = document.createElement("img");
     listElement.appendChild(imgElement);
-    imgElement.setAttribute('src', 'cards/' + hand[i] + '.png');
-    listElement.classList.add('cards');
+    imgElement.setAttribute("src", "cards/" + hand[lastIndex] + ".png");
+    listElement.classList.add("cards");
   }
-};
-// Döpte om Deal - button i spelet och slog ihop funktionen i deal.
-if (dealButton) {
-  dealButton.addEventListener('click', deal);
 }
 
-let stay = function () {
-  if (endGame === false) {
+dealButton?.addEventListener("click", deal);
+
+function stay() {
+  if (startGame && !endGame) {
     do {
-      let card = draw(shuffledDeck);
+      const card = draw(shuffledDeck);
       dealerHand.push(card);
       dealerHandPoints.push(card);
       dealerResult = score(dealerHandPoints);
-      if (dealerInputField) {
-        dealerInputField.setAttribute('value', dealerResult.toString());
-      }
+      dealerInputField?.setAttribute("value", dealerResult.toString());
 
-      let i = dealerHand.length - 1;
-      dealerHand[i] = prettyCard(dealerHand[i]);
-      dealerListElement = document.createElement('li');
-      if (dealerOList) {
-        dealerOList.appendChild(dealerListElement);
-      }
-      dealerImgElement = document.createElement('img');
+      const lastIndex = dealerHand.length - 1;
+      dealerHand[lastIndex] = prettyCard(dealerHand[lastIndex]);
+      dealerListElement = document.createElement("li");
+      dealerOList?.appendChild(dealerListElement);
+
+      dealerImgElement = document.createElement("img");
       dealerListElement.appendChild(dealerImgElement);
-      dealerImgElement.setAttribute('src', 'cards/' + dealerHand[i] + '.png');
+      dealerImgElement.setAttribute(
+        "src",
+        "cards/" + dealerHand[lastIndex] + ".png",
+      );
     } while (dealerResult < 21 && dealerResult < result);
 
     if (result === 21 && hand.length === 2) {
@@ -269,24 +259,21 @@ let stay = function () {
     } else {
       updatePoints(1);
     }
+    startGame = false;
+    endGame = true;
   }
   if (earnedPoints === 2) {
-    alert('You are the champion!');
+    alert("You are the champion!");
   }
-  endGame = true;
-};
-if (stayButton) {
-  stayButton.addEventListener('click', stay);
 }
 
-let updatePoints = function (change: number) {
-  earnedPoints += change;
-  sessionStorage.setItem(SCORE_KEY, earnedPoints.toString());
-  //points.value = earnedPoints; // Update the visible value directly
-};
+stayButton?.addEventListener("click", stay);
 
-let clearTable = function () {
+function updatePoints(change: number) {
+  earnedPoints += change;
+  console.log(earnedPoints);
+  if (scoreField instanceof HTMLInputElement) {
+    scoreField.value = earnedPoints.toString();
+  }
   sessionStorage.setItem(SCORE_KEY, earnedPoints.toString());
-  location.reload();
-  endGame = false;
-};
+}
